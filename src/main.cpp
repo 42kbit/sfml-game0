d41b4ff09a0bd5c1e7a7bc3f8d6e7d011c901c84
Ostrikov Serhii <serostrikov@gmail.com>
2021-10-07 17:23:02 +0300

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,34 @@
-#include <SFML/Graphics.hpp>
+#include "SFML/Graphics.hpp"
+#include "Global/Configuration.h"
+
+#include <iostream>
 
 int main()
 {
+    sf::Clock deltaTimer;
+
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::Event event;
+
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
+    Configuration::window = &window;
+    Configuration::event = &event;
+
+    window.setVerticalSyncEnabled(1);
+
     while (window.isOpen())
     {
-        sf::Event event;
+        std::cout << "DeltaTime: " << Configuration::get_deltaTime() << std::endl;
+        
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        Configuration::dt_clock.restart();
 
         window.clear();
         window.draw(shape);
